06923e432f8d366cbbffb2f4eb89aef4a7497486
Eric Biggers <ebiggers3@gmail.com>
2015-12-03 23:42:09 -0600

    libdeflate.h: update comment about compression levels

--- libdeflate.h
+++ libdeflate.h
@@ -20,9 +20,10 @@ struct deflate_compressor;
 
 /*
  * deflate_alloc_compressor() allocates a new DEFLATE compressor.
- * 'compression_level' is the compression level on a zlib-like scale (1 =
- * fastest, 6 = medium/default, 9 = slowest).  The return value is a pointer to
- * the new DEFLATE compressor, or NULL if out of memory.
+ * 'compression_level' is the compression level on a zlib-like scale but with a
+ * higher maximum value (1 = fastest, 6 = medium/default, 9 = slow, 12 =
+ * slowest).  The return value is a pointer to the new DEFLATE compressor, or
+ * NULL if out of memory.
  *
  * Note: for compression, the sliding window size is defined at compilation time
  * to 32768, the largest size permissible in the DEFLATE format.  It cannot be
