de7c13c65947ad58066353bf003fb74f8fa07cc7
Eric Biggers <ebiggers3@gmail.com>
2015-12-04 23:54:22 -0600

    Add MIPS test script

---
+++ tools/mips_test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+TOOLCHAIN_DIR=$HOME/src/ddwrt-toolchains/toolchain-mips_34kc_gcc-5.1.0_musl-1.1.9
+
+make clean
+make -j4 BUILD_SHARED_LIBRARY=no BUILD_BENCHMARK_PROGRAM=yes \
+	CC="$TOOLCHAIN_DIR/bin/mips-openwrt-linux-musl-gcc" \
+	CFLAGS="-DNEED_PRINTF"
+
+scp benchmark root@dd-wrt:
+ssh root@dd-wrt ./benchmark a "$@"
