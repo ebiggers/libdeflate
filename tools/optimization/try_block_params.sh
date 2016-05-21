bc02ba9d2ff47a1f197c976e2baeee5fae7ce637
Eric Biggers <ebiggers3@gmail.com>
2016-05-20 21:03:07 -0500

    Optimization 2

--- tools/optimization/try_block_params.sh
+++ tools/optimization/try_block_params.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$LEVEL" -o -z "$INPUTFILE" ]; then
+	echo "Must specify LEVEL and INPUTFILE" 1>&2
+	exit 1
+fi
+
 topdir="$(dirname "$0")/../.."
 
 git checkout -f "$topdir/src" > /dev/null
@@ -10,11 +15,9 @@ sed -i -e	\
 "
     s/\<block_size >> 12\>/block_size >> $BLOCK_SIZE_SHIFT/
     s/\<200 \* stats->num_observations/$BLOCK_CUTOFF * stats->num_observations/
-    s/#define MIN_BLOCK_LENGTH.*$/#define MIN_BLOCK_LENGTH $MIN_BLOCK_LENGTH/
-    s/#define MAX_BLOCK_LENGTH.*$/#define MAX_BLOCK_LENGTH $MAX_BLOCK_LENGTH/
     s/num_new_observations < 512\>/num_new_observations < $OBSERVATIONS_PER_CHECK/
 "	\
 "$topdir/src/deflate_compress.c"
 
 make -C "$topdir" -j BUILD_BENCHMARK_PROGRAM=yes > /dev/null
-"$topdir/benchmark" -l 12 -s 100000000 $HOME/data/testdata | grep Compressed | cut -f 4 -d ' '
+"$topdir/benchmark" -l $LEVEL -s 100000000 $INPUTFILE | grep Compressed | cut -f 4 -d ' '
