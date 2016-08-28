e603f7868db7c63bae0bcfa5ee07cd3de328dc36
Eric Biggers <ebiggers3@gmail.com>
2016-08-27 23:38:59 -0700

    aligned_malloc: store pointer directly

--- lib/aligned_malloc.c
+++ lib/aligned_malloc.c
@@ -25,15 +25,11 @@
 void *
 aligned_malloc(size_t alignment, size_t size)
 {
-	const uintptr_t mask = alignment - 1;
-	char *ptr = NULL;
-	char *raw_ptr;
-
-	raw_ptr = malloc(mask + sizeof(size_t) + size);
-	if (raw_ptr) {
-		ptr = (char *)raw_ptr + sizeof(size_t);
-		ptr = (void *)(((uintptr_t)ptr + mask) & ~mask);
-		*((size_t *)ptr - 1) = ptr - raw_ptr;
+	void *ptr = malloc(sizeof(void *) + alignment - 1 + size);
+	if (ptr) {
+		void *orig_ptr = ptr;
+		ptr = (void *)ALIGN((uintptr_t)ptr + sizeof(void *), alignment);
+		((void **)ptr)[-1] = orig_ptr;
 	}
 	return ptr;
 }
@@ -42,5 +38,5 @@ void
 aligned_free(void *ptr)
 {
 	if (ptr)
-		free((char *)ptr - *((size_t *)ptr - 1));
+		free(((void **)ptr)[-1]);
 }
