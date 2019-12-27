91e972e864cc7d6bbb2d211d6a18df0ff1b34066
Eric Biggers <ebiggers3@gmail.com>
2019-12-27 10:48:44 -0600

    v1.4

--- libdeflate.h
+++ libdeflate.h
@@ -10,8 +10,8 @@ extern "C" {
 #endif
 
 #define LIBDEFLATE_VERSION_MAJOR	1
-#define LIBDEFLATE_VERSION_MINOR	3
-#define LIBDEFLATE_VERSION_STRING	"1.3"
+#define LIBDEFLATE_VERSION_MINOR	4
+#define LIBDEFLATE_VERSION_STRING	"1.4"
 
 #include <stddef.h>
 #include <stdint.h>
