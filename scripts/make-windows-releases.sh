61583588cdf062662401fb26a3fa8252be7c5578
Eric Biggers <ebiggers3@gmail.com>
2020-10-18 14:21:25 -0700

    scripts/make-window-releases.sh: address all shellcheck warnings
    Also add '-o pipefail'.

--- scripts/make-windows-releases.sh
+++ scripts/make-windows-releases.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-set -eu
+set -eu -o pipefail
 
 for arch in 'i686' 'x86_64'; do
 	make clean
 	make -j CC=${arch}-w64-mingw32-gcc CFLAGS="-Werror" all \
 		benchmark.exe checksum.exe
 	dir=libdeflate-$(git describe --tags | tr -d v)-windows-${arch}-bin
-	rm -rf $dir ${dir}.zip
-	mkdir $dir
-	cp libdeflate.{dll,lib,def} libdeflatestatic.lib libdeflate.h *.exe $dir
-	${arch}-w64-mingw32-strip ${dir}/libdeflate.dll ${dir}/*.exe
+	rm -rf "$dir" "$dir.zip"
+	mkdir "$dir"
+	cp libdeflate.{dll,lib,def} libdeflatestatic.lib libdeflate.h ./*.exe \
+		"$dir"
+	${arch}-w64-mingw32-strip "$dir/libdeflate.dll" "$dir"/*.exe
 	for file in COPYING NEWS; do
-		sed < $file > ${dir}/${file}.txt -e 's/$/\r/g'
+		sed < $file > "$dir/${file}.txt" -e 's/$/\r/g'
 	done
-	for file in README.md; do
-		sed < $file > ${dir}/${file} -e 's/$/\r/g'
-	done
-	(cd ${dir} && zip -r ../${dir}.zip .)
+	sed < README.md > "$dir/README.md" -e 's/$/\r/g'
+	(cd "$dir" && zip -r "../${dir}.zip" .)
 done
