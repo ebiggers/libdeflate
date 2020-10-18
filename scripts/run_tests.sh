0a9254fb496284856bef670dfe89420d7c41ddc1
Eric Biggers <ebiggers3@gmail.com>
2020-10-18 14:21:26 -0700

    scripts/run_tests.sh: refactor and allow specifying CC and CFLAGS
    Don't try to detect and use different compilers, since it's better to
    specify this via the environment (e.g. via the Travis CI build matrix).
    
    While doing this, also deduplicate the logic for testing with valgrind
    and UBSAN, improve the log messages, and add a test with -O3.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -10,9 +10,17 @@ if [ $# -ne 0 ]; then
 	exit 2
 fi
 
+# Use CC if specified in environment, else default to "cc".
+: "${CC:=cc}"
+
+# Use CFLAGS if specified in environment.
+: "${CFLAGS:=}"
+
+# Use TESTDATA if specified in environment, else generate it.
 if [ -z "${TESTDATA:-}" ]; then
 	# Generate default TESTDATA file.
 	TESTDATA=$(mktemp -t libdeflate_testdata.XXXXXXXXXX)
+	export TESTDATA
 	trap 'rm -f "$TESTDATA"' EXIT
 	find . '(' -name '*.c' -o -name '*.h' -o -name '*.sh' ')' \
 		-exec cat '{}' ';' | head -c 1000000 > "$TESTDATA"
@@ -20,13 +28,27 @@ fi
 
 MAKE="make -j$(getconf _NPROCESSORS_ONLN)"
 
-VALGRIND="valgrind --quiet --error-exitcode=100 --leak-check=full --errors-for-leak-kinds=all"
-SANITIZE_CFLAGS="-fsanitize=undefined -fno-sanitize-recover=undefined,integer"
+CC_VERSION=$($CC --version | head -1)
+
+ARCH=$(uname -m)
 
 ###############################################################################
 
+INDENT=0
+
 log() {
-	echo "[$(date)] $@"
+	echo -n "[$(date)] "
+	head -c $(( INDENT * 4 )) /dev/zero | tr '\0' ' '
+	echo "$@"
+}
+
+begin() {
+	log "$@"
+	(( INDENT++ )) || true
+}
+
+end() {
+	(( INDENT-- )) || true
 }
 
 run_cmd() {
@@ -34,9 +56,30 @@ run_cmd() {
 	"$@" > /dev/null
 }
 
-###############################################################################
+cflags_supported() {
+	# -Werror is needed here in order for old versions of clang to reject
+	# invalid options.
+	echo 'int main(void){ return 0; }' \
+		| $CC $CFLAGS "$@" -Werror -x c - -o /dev/null 2>/dev/null
+}
 
-native_build_and_test() {
+valgrind_version_at_least() {
+	local want_vers=$1
+	local vers
+
+	vers=$(valgrind --version | grep -E -o '[0-9\.]+' | head -1)
+
+	[ "$want_vers" = "$(echo -e "$vers\n$want_vers" | sort -V | head -1)" ]
+}
+
+build_and_run_tests() {
+	local quick=false
+	if [ "${1:-}" = "--quick" ]; then
+		quick=true
+		shift
+	fi
+
+	begin "CC=$CC CFLAGS=\"$CFLAGS\" WRAPPER=\"$WRAPPER\" $*"
 
 	# Build libdeflate, including the test programs.  Set the special test
 	# support flag to get support for LIBDEFLATE_DISABLE_CPU_FEATURES.
@@ -51,8 +94,8 @@ native_build_and_test() {
 	# newest to oldest for each architecture, cumulatively.  In practice,
 	# that's good enough to cover all the code.
 	local features=('')
-	if ! [[ "$*" =~ "-march=native" ]]; then
-		case "$(uname -m)" in
+	if ! [[ "$CFLAGS" =~ "-march=native" ]] && ! $quick; then
+		case "$ARCH" in
 		i386|x86_64)
 			features+=(avx512bw avx2 avx bmi2 pclmul sse2)
 			;;
@@ -69,53 +112,20 @@ native_build_and_test() {
 				disable_str+=","
 			fi
 			disable_str+="$feature"
-			log "Retrying with CPU features disabled: $disable_str"
 		fi
-		WRAPPER="$WRAPPER" TESTDATA="$TESTDATA" \
-			LIBDEFLATE_DISABLE_CPU_FEATURES="$disable_str" \
-			sh ./scripts/exec_tests.sh > /dev/null
+		log "Using LIBDEFLATE_DISABLE_CPU_FEATURES=$disable_str"
+		LIBDEFLATE_DISABLE_CPU_FEATURES="$disable_str" \
+		    sh ./scripts/exec_tests.sh > /dev/null
 	done
+	end
 }
 
-native_tests() {
-	local compiler compilers_to_try=(gcc)
-	local cflags cflags_to_try=("")
-	shopt -s nullglob
-	compilers_to_try+=(/usr/bin/gcc-[0-9]*)
-	compilers_to_try+=(/usr/bin/clang-[0-9]*)
-	compilers_to_try+=(/opt/gcc*/bin/gcc)
-	compilers_to_try+=(/opt/clang*/bin/clang)
-	shopt -u nullglob
-
-	if [ "$(uname -m)" = "x86_64" ]; then
-		cflags_to_try+=("-march=native")
-		cflags_to_try+=("-m32")
+verify_freestanding_build() {
+	# It is expected that sanitizer builds link to external functions.
+	if [[ "$CFLAGS" =~ "-fsanitize" ]]; then
+		return 0
 	fi
-	for compiler in ${compilers_to_try[@]}; do
-		for cflags in "${cflags_to_try[@]}"; do
-			if [ "$cflags" = "-m32" ] && \
-			   $compiler -v |& grep -q -- '--disable-multilib'
-			then
-				continue
-			fi
-			log "Running tests with CC=$compiler," \
-				"CFLAGS=$cflags"
-			WRAPPER= native_build_and_test \
-				CC=$compiler CFLAGS="$cflags -Werror"
-		done
-	done
-
-	log "Running tests with Valgrind"
-	WRAPPER="$VALGRIND" native_build_and_test
-
-	log "Running tests with undefined behavior sanitizer"
-	WRAPPER= native_build_and_test CC=clang CFLAGS="$SANITIZE_CFLAGS"
-}
-
-# Test the library built with FREESTANDING=1.
-freestanding_tests() {
-
-	WRAPPER= native_build_and_test FREESTANDING=1
+	log "Verifying that freestanding build is really freestanding"
 	if nm libdeflate.so | grep -q ' U '; then
 		echo 1>&2 "Freestanding lib links to external functions!:"
 		nm libdeflate.so | grep ' U '
@@ -126,42 +136,75 @@ freestanding_tests() {
 		ldd libdeflate.so
 		return 1
 	fi
-
-	WRAPPER="$VALGRIND" native_build_and_test FREESTANDING=1
-
-	WRAPPER= native_build_and_test FREESTANDING=1 \
-		CC=clang CFLAGS="$SANITIZE_CFLAGS"
 }
 
-###############################################################################
-
 gzip_tests() {
-
+	local gzips=("$PWD/gzip")
+	local gunzips=("$PWD/gunzip")
+	if [ "${1:-}" != "--quick" ]; then
+		gzips+=(/bin/gzip)
+		gunzips+=(/bin/gunzip)
+	fi
 	local gzip gunzip
-	run_cmd $MAKE gzip gunzip
-	for gzip in "$PWD/gzip" /bin/gzip; do
-		for gunzip in "$PWD/gunzip" /bin/gunzip; do
-			log "Running gzip program tests with GZIP=$gzip," \
-				"GUNZIP=$gunzip"
+
+	begin "Running gzip program tests with CC=\"$CC\" CFLAGS=\"$CFLAGS\""
+	$MAKE gzip gunzip > /dev/null
+	for gzip in "${gzips[@]}"; do
+		for gunzip in "${gunzips[@]}"; do
+			log "GZIP=$gzip, GUNZIP=$gunzip"
 			GZIP="$gzip" GUNZIP="$gunzip" TESTDATA="$TESTDATA" \
 				./scripts/gzip_tests.sh
 		done
 	done
+	end
+}
 
-	log "Running gzip program tests with Valgrind"
-	GZIP="$VALGRIND $PWD/gzip" GUNZIP="$VALGRIND $PWD/gunzip" \
-		TESTDATA="$TESTDATA" ./scripts/gzip_tests.sh
+do_run_tests() {
+	build_and_run_tests "$@"
+	if [ "${1:-}" != "--quick" ]; then
+		build_and_run_tests FREESTANDING=1
+		verify_freestanding_build
+	fi
+	gzip_tests "$@"
+}
 
-	log "Running gzip program tests with undefined behavior sanitizer"
-	run_cmd $MAKE CC=clang CFLAGS="$SANITIZE_CFLAGS" gzip gunzip
-	GZIP="$PWD/gzip" GUNZIP="$PWD/gunzip" \
-		TESTDATA="$TESTDATA" ./scripts/gzip_tests.sh
+run_tests() {
+	export WRAPPER="" # no wrapper by default; overridden by valgrind tests
+	local cflags
+
+	begin "Running tests"
+	do_run_tests
+	end
+
+	cflags=("-O3")
+	if cflags_supported "${cflags[@]}" "-march=native"; then
+		cflags+=("-march=native")
+	fi
+	begin "Running tests with ${cflags[*]}"
+	CFLAGS="$CFLAGS ${cflags[*]}" do_run_tests
+	end
+
+	# Need valgrind 3.9.0 for '--errors-for-leak-kinds=all'
+	# Need valgrind 3.12.0 for armv8 crypto and crc instructions
+	if valgrind_version_at_least 3.12.0; then
+		begin "Running tests with Valgrind"
+		WRAPPER="valgrind --quiet --error-exitcode=100 --leak-check=full --errors-for-leak-kinds=all" \
+			do_run_tests --quick
+		end
+	fi
+
+	cflags=("-fsanitize=undefined" "-fno-sanitize-recover=undefined")
+	if cflags_supported "${cflags[@]}"; then
+		begin "Running tests with UBSAN"
+		CFLAGS="$CFLAGS ${cflags[*]}" do_run_tests --quick
+		end
+	else
+		log "Skipping UBSAN tests because compiler ($CC_VERSION) doesn't support UBSAN"
+	fi
 }
 
 ###############################################################################
 
 log "Starting libdeflate tests"
-native_tests
-freestanding_tests
-gzip_tests
+run_tests
 log "All tests passed!"
