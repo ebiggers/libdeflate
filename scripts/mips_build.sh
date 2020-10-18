df00350bf9bb4422254cb241b6b530295a86d795
Eric Biggers <ebiggers3@gmail.com>
2020-10-18 14:21:25 -0700

    scripts: remove mips_tests
    This was only useful for me to do local testing, I don't have the needed
    MIPS router available anymore, and its main purpose was test a big
    endian system but that's now covered by testing s390x with Travis CI.

--- scripts/mips_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-TOOLCHAIN_DIR=$HOME/src/ddwrt-toolchains/toolchain-mips_34kc_gcc-5.1.0_musl-1.1.9
-
-make -j$(grep -c processor /proc/cpuinfo) all test_programs \
-	CC="$TOOLCHAIN_DIR/bin/mips-openwrt-linux-musl-gcc" \
-	CFLAGS="-DNEED_PRINTF -Werror"
