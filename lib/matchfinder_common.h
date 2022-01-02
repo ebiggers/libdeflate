04105f0977c875c354ed46ae10b039c67c233094
Eric Biggers <ebiggers3@gmail.com>
2022-01-01 19:49:14 -0600

    matchfinder: add MATCHFINDER_ALIGNED macro
    Avoid some code duplication.

--- lib/matchfinder_common.h
+++ lib/matchfinder_common.h
@@ -28,11 +28,14 @@ typedef s16 mf_pos_t;
 #undef matchfinder_init
 #undef matchfinder_rebase
 #ifdef _aligned_attribute
+#  define MATCHFINDER_ALIGNED _aligned_attribute(MATCHFINDER_MEM_ALIGNMENT)
 #  if defined(__arm__) || defined(__aarch64__)
 #    include "arm/matchfinder_impl.h"
 #  elif defined(__i386__) || defined(__x86_64__)
 #    include "x86/matchfinder_impl.h"
 #  endif
+#else
+#  define MATCHFINDER_ALIGNED
 #endif
 
 /*
