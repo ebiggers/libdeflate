301bbe789e8483d7ed6008e13395b5d597430c59
Eric Biggers <ebiggers3@gmail.com>
2022-11-28 23:43:51 -0800

    scripts: make benchmark and checksum scripts quieter

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -5,6 +5,6 @@ set -e
 SCRIPTDIR="$(dirname "$0")"
 BUILDDIR="$SCRIPTDIR/../build"
 
-"$SCRIPTDIR"/cmake-helper.sh -DLIBDEFLATE_BUILD_TESTS=1 -G Ninja
-ninja -C "$BUILDDIR" checksum
+"$SCRIPTDIR"/cmake-helper.sh -DLIBDEFLATE_BUILD_TESTS=1 -G Ninja > /dev/null
+ninja -C "$BUILDDIR" --quiet checksum
 "$BUILDDIR"/programs/checksum "$@"
