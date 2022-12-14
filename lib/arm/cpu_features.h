ebiggers/libdeflate

commit 84c76f6f2cf5e5ac4deca0b1c25faebb42469f58

lib/arm: make crc32 code work with MSVC
Rework this code to avoid gcc vector extensions, and once that is done
allow it to be built with MSVC (and clang in MSVC compatibility mode).

lib/arm/cpu_features.h
@@ -43,13 +43,6 @@
 #  define HAVE_DYNAMIC_ARM_CPU_FEATURES	1
 #endif
 
-#ifdef __GNUC__
-#  define HAVE_INTRIN	1
-#else
-/* intrinsics not compatible (e.g. MSVC, or clang in MSVC mode) */
-#  define HAVE_INTRIN	0
-#endif
-
 #define ARM_CPU_FEATURE_NEON		0x00000001
 #define ARM_CPU_FEATURE_PMULL		0x00000002
 #define ARM_CPU_FEATURE_CRC32		0x00000004
@@ -102,18 +95,24 @@ static inline u32 get_arm_cpu_features(void) { return 0; }
 #else
 #  define HAVE_PMULL_NATIVE	0
 #endif
-#define HAVE_PMULL_TARGET \
+#if HAVE_PMULL_NATIVE || \
 	(HAVE_DYNAMIC_ARM_CPU_FEATURES && \
-	 (GCC_PREREQ(6, 1) || __has_builtin(__builtin_neon_vmull_p64)))
-/*
- * On arm32 with clang, the crypto intrinsics (which include pmull) are not
- * defined, even when using -mfpu=crypto-neon-fp-armv8, because clang's
- * <arm_neon.h> puts their definitions behind __aarch64__.
- */
-#if HAVE_NEON_INTRIN && (HAVE_PMULL_NATIVE || HAVE_PMULL_TARGET) && \
-	!(defined(ARCH_ARM32) && defined(__clang__)) && \
-	CPU_IS_LITTLE_ENDIAN() /* pmull code on big endian is untested */
-#  define HAVE_PMULL_INTRIN	1
+	 (GCC_PREREQ(6, 1) || __has_builtin(__builtin_neon_vmull_p64) || \
+	  defined(_MSC_VER)) && \
+	  /*
+	   * On arm32 with clang, the crypto intrinsics (which include pmull)
+	   * are not defined, even when using -mfpu=crypto-neon-fp-armv8,
+	   * because clang's <arm_neon.h> puts their definitions behind
+	   * __aarch64__.
+	   */ \
+	 !(defined(ARCH_ARM32) && defined(__clang__)))
+#  define HAVE_PMULL_INTRIN	CPU_IS_LITTLE_ENDIAN() /* untested on big endian */
+   /* Work around MSVC's vmull_p64() taking poly64x1_t instead of poly64_t */
+#  ifdef _MSC_VER
+#    define compat_vmull_p64(a, b)  vmull_p64(vcreate_p64(a), vcreate_p64(b))
+#  else
+#    define compat_vmull_p64(a, b)  vmull_p64((a), (b))
+#  endif
 #else
 #  define HAVE_PMULL_INTRIN	0
 #endif
@@ -124,9 +123,6 @@ static inline u32 get_arm_cpu_features(void) { return 0; }
 #else
 #  define HAVE_CRC32_NATIVE	0
 #endif
-#define HAVE_CRC32_TARGET \
-	(HAVE_DYNAMIC_ARM_CPU_FEATURES && \
-	 (GCC_PREREQ(4, 9) || __has_builtin(__builtin_arm_crc32b)))
 /*
  * Support for ARM CRC32 intrinsics when CRC32 instructions are not enabled in
  * the main target has been affected by two gcc bugs, which we must avoid by
@@ -139,16 +135,20 @@ static inline u32 get_arm_cpu_features(void) { return 0; }
  * We use the second set of prerequisites, as they are stricter and we have no
  * way to detect the binutils version directly from a C source file.
  */
-#define HAVE_CRC32_INTRIN \
-	(HAVE_INTRIN && \
-	 (HAVE_CRC32_NATIVE || (HAVE_CRC32_TARGET && \
-				(!GCC_PREREQ(1, 0) || \
-				 GCC_PREREQ(11, 3) || \
-				 (GCC_PREREQ(10, 4) && !GCC_PREREQ(11, 0)) || \
-				 (GCC_PREREQ(9, 5) && !GCC_PREREQ(10, 0))))))
+#if HAVE_CRC32_NATIVE || \
+	(HAVE_DYNAMIC_ARM_CPU_FEATURES && \
+	 (__has_builtin(__builtin_arm_crc32b) || \
+	  GCC_PREREQ(11, 3) || \
+	  (GCC_PREREQ(10, 4) && !GCC_PREREQ(11, 0)) || \
+	  (GCC_PREREQ(9, 5) && !GCC_PREREQ(10, 0)) || \
+	  defined(_MSC_VER)))
+#  define HAVE_CRC32_INTRIN	1
+#else
+#  define HAVE_CRC32_INTRIN	0
+#endif
 
 /* SHA3 (needed for the eor3 instruction) */
-#ifdef ARCH_ARM64
+#if defined(ARCH_ARM64) && !defined(_MSC_VER)
 #  ifdef __ARM_FEATURE_SHA3
 #    define HAVE_SHA3_NATIVE	1
 #  else

lib/arm/crc32_impl.h
@@ -63,7 +63,9 @@
 #    endif
 #  endif
 
-#include <arm_acle.h>
+#ifndef _MSC_VER
+#  include <arm_acle.h>
+#endif
 
 /*
  * Combine the CRCs for 4 adjacent chunks of length L = CRC32_FIXED_CHUNK_LEN
@@ -233,9 +235,7 @@ crc32_arm_crc(u32 crc, const u8 *p, size_t len)
  * checksummed chunks, not for folding the data itself.  See crc32_arm_pmull*()
  * for implementations that use pmull for folding the data itself.
  */
-#if HAVE_CRC32_INTRIN && HAVE_PMULL_INTRIN && \
-	((HAVE_CRC32_NATIVE && HAVE_PMULL_NATIVE) || \
-	 (HAVE_CRC32_TARGET && HAVE_PMULL_TARGET))
+#if HAVE_CRC32_INTRIN && HAVE_PMULL_INTRIN
 #  if HAVE_CRC32_NATIVE && HAVE_PMULL_NATIVE
 #    define ATTRIBUTES
 #  else
@@ -250,8 +250,20 @@ crc32_arm_crc(u32 crc, const u8 *p, size_t len)
 #    endif
 #  endif
 
-#include <arm_acle.h>
+#ifndef _MSC_VER
+#  include <arm_acle.h>
+#endif
 #include <arm_neon.h>
+
+/* Do carryless multiplication of two 32-bit values. */
+static forceinline ATTRIBUTES u64
+clmul_u32(u32 a, u32 b)
+{
+	uint64x2_t res = vreinterpretq_u64_p128(
+				compat_vmull_p64((poly64_t)a, (poly64_t)b));
+
+	return vgetq_lane_u64(res, 0);
+}
 
 /*
  * Like combine_crcs_slow(), but uses vmull_p64 to do the multiplications more
@@ -262,9 +274,9 @@ crc32_arm_crc(u32 crc, const u8 *p, size_t len)
 static forceinline ATTRIBUTES u32
 combine_crcs_fast(u32 crc0, u32 crc1, u32 crc2, u32 crc3, size_t i)
 {
-	u64 res0 = vmull_p64(crc0, crc32_mults_for_chunklen[i][0]);
-	u64 res1 = vmull_p64(crc1, crc32_mults_for_chunklen[i][1]);
-	u64 res2 = vmull_p64(crc2, crc32_mults_for_chunklen[i][2]);
+	u64 res0 = clmul_u32(crc0, crc32_mults_for_chunklen[i][0]);
+	u64 res1 = clmul_u32(crc1, crc32_mults_for_chunklen[i][1]);
+	u64 res2 = clmul_u32(crc2, crc32_mults_for_chunklen[i][2]);
 
 	return __crc32d(0, res0 ^ res1 ^ res2) ^ crc3;
 }
@@ -445,17 +457,25 @@ crc32_arm_crc_pmullcombine(u32 crc, const u8 *p, size_t len)
 static u32 ATTRIBUTES MAYBE_UNUSED
 crc32_arm_pmullx4(u32 crc, const u8 *p, size_t len)
 {
-	const poly64x2_t multipliers_4 = (poly64x2_t)CRC32_4VECS_MULTS;
-	const poly64x2_t multipliers_2 = (poly64x2_t)CRC32_2VECS_MULTS;
-	const poly64x2_t multipliers_1 = (poly64x2_t)CRC32_1VECS_MULTS;
-	const uint8x16_t zeroes = (uint8x16_t){ 0 };
-	const uint8x16_t mask32 = (uint8x16_t)(uint32x4_t){ 0xFFFFFFFF };
+	static const u64 _aligned_attribute(16) mults[3][2] = {
+		CRC32_1VECS_MULTS,
+		CRC32_4VECS_MULTS,
+		CRC32_2VECS_MULTS,
+	};
+	static const u64 _aligned_attribute(16) final_mults[3][2] = {
+		{ CRC32_FINAL_MULT, 0 },
+		{ CRC32_BARRETT_CONSTANT_1, 0 },
+		{ CRC32_BARRETT_CONSTANT_2, 0 },
+	};
+	const uint8x16_t zeroes = vdupq_n_u8(0);
+	const uint8x16_t mask32 = vreinterpretq_u8_u64(vdupq_n_u64(0xFFFFFFFF));
+	const poly64x2_t multipliers_1 = load_multipliers(mults[0]);
 	uint8x16_t v0, v1, v2, v3;
 
 	if (len < 64 + 15) {
 		if (len < 16)
 			return crc32_slice1(crc, p, len);
-		v0 = vld1q_u8(p) ^ (uint8x16_t)(uint32x4_t){ crc };
+		v0 = veorq_u8(vld1q_u8(p), u32_to_bytevec(crc));
 		p += 16;
 		len -= 16;
 		while (len >= 16) {
@@ -464,10 +484,12 @@ crc32_arm_pmullx4(u32 crc, const u8 *p, size_t len)
 			len -= 16;
 		}
 	} else {
+		const poly64x2_t multipliers_4 = load_multipliers(mults[1]);
+		const poly64x2_t multipliers_2 = load_multipliers(mults[2]);
 		const size_t align = -(uintptr_t)p & 15;
 		const uint8x16_t *vp;
 
-		v0 = vld1q_u8(p) ^ (uint8x16_t)(uint32x4_t){ crc };
+		v0 = veorq_u8(vld1q_u8(p), u32_to_bytevec(crc));
 		p += 16;
 		/* Align p to the next 16-byte boundary. */
 		if (align) {
@@ -508,21 +530,19 @@ crc32_arm_pmullx4(u32 crc, const u8 *p, size_t len)
 	 * which is equivalent to multiplying by x^32.  This is needed because
 	 * the CRC is defined as M(x)*x^32 mod G(x), not just M(x) mod G(x).
 	 */
-	v0 = vextq_u8(v0, zeroes, 8) ^
-	     (uint8x16_t)vmull_p64((poly64_t)vget_low_u8(v0),
-				   CRC32_1VECS_MULT_2);
+
+	v0 = veorq_u8(vextq_u8(v0, zeroes, 8),
+		      clmul_high(vextq_u8(zeroes, v0, 8), multipliers_1));
 
 	/* Fold 96 => 64 bits. */
-	v0 = vextq_u8(v0, zeroes, 4) ^
-		(uint8x16_t)vmull_p64((poly64_t)vget_low_u8(v0 & mask32),
-				      CRC32_FINAL_MULT);
+	v0 = veorq_u8(vextq_u8(v0, zeroes, 4),
+		      clmul_low(vandq_u8(v0, mask32),
+				load_multipliers(final_mults[0])));
 
 	/* Reduce 64 => 32 bits using Barrett reduction. */
-	v1 = (uint8x16_t)vmull_p64((poly64_t)vget_low_u8(v0 & mask32),
-				   CRC32_BARRETT_CONSTANT_1);
-	v1 = (uint8x16_t)vmull_p64((poly64_t)vget_low_u8(v1 & mask32),
-				   CRC32_BARRETT_CONSTANT_2);
-	return ((uint32x4_t)(v0 ^ v1))[1];
+	v1 = clmul_low(vandq_u8(v0, mask32), load_multipliers(final_mults[1]));
+	v1 = clmul_low(vandq_u8(v1, mask32), load_multipliers(final_mults[2]));
+	return vgetq_lane_u32(vreinterpretq_u32_u8(veorq_u8(v0, v1)), 1);
 }
 #undef SUFFIX
 #undef ATTRIBUTES
@@ -535,9 +555,7 @@ crc32_arm_pmullx4(u32 crc, const u8 *p, size_t len)
  *
  * See crc32_pmull_wide.h for explanation.
  */
-#if defined(ARCH_ARM64) && HAVE_PMULL_INTRIN && HAVE_CRC32_INTRIN && \
-	((HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE) || \
-	 (HAVE_PMULL_TARGET && HAVE_CRC32_TARGET))
+#if defined(ARCH_ARM64) && HAVE_PMULL_INTRIN && HAVE_CRC32_INTRIN
 #  define crc32_arm_pmullx12_crc	crc32_arm_pmullx12_crc
 #  define SUFFIX				 _pmullx12_crc
 #  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE
@@ -563,8 +581,7 @@ crc32_arm_pmullx4(u32 crc, const u8 *p, size_t len)
  * HAVE_SHA3_INTRIN, as we have an inline asm fallback for eor3.
  */
 #if defined(ARCH_ARM64) && HAVE_PMULL_INTRIN && HAVE_CRC32_INTRIN && \
-	((HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && HAVE_SHA3_NATIVE) || \
-	 (HAVE_PMULL_TARGET && HAVE_CRC32_TARGET && HAVE_SHA3_TARGET))
+	(HAVE_SHA3_TARGET || HAVE_SHA3_NATIVE)
 #  define crc32_arm_pmullx12_crc_eor3	crc32_arm_pmullx12_crc_eor3
 #  define SUFFIX				 _pmullx12_crc_eor3
 #  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && HAVE_SHA3_NATIVE

lib/arm/crc32_pmull_helpers.h
@@ -39,21 +39,51 @@
 
 #include <arm_neon.h>
 
+/* Create a vector with 'a' in the first 4 bytes, and the rest zeroed out. */
+#undef u32_to_bytevec
+static forceinline ATTRIBUTES uint8x16_t
+ADD_SUFFIX(u32_to_bytevec)(u32 a)
+{
+	return vreinterpretq_u8_u32(vsetq_lane_u32(a, vdupq_n_u32(0), 0));
+}
+#define u32_to_bytevec	ADD_SUFFIX(u32_to_bytevec)
+
+/* Load two 64-bit values into a vector. */
+#undef load_multipliers
+static forceinline ATTRIBUTES poly64x2_t
+ADD_SUFFIX(load_multipliers)(const u64 p[2])
+{
+	return vreinterpretq_p64_u64(vld1q_u64(p));
+}
+#define load_multipliers	ADD_SUFFIX(load_multipliers)
+
+/* Do carryless multiplication of the low halves of two vectors. */
+#undef clmul_low
+static forceinline ATTRIBUTES uint8x16_t
+ADD_SUFFIX(clmul_low)(uint8x16_t a, poly64x2_t b)
+{
+	return vreinterpretq_u8_p128(
+		     compat_vmull_p64(vgetq_lane_p64(vreinterpretq_p64_u8(a), 0),
+				      vgetq_lane_p64(b, 0)));
+}
+#define clmul_low	ADD_SUFFIX(clmul_low)
+
+/* Do carryless multiplication of the high halves of two vectors. */
 #undef clmul_high
-static forceinline ATTRIBUTES poly128_t
-ADD_SUFFIX(clmul_high)(poly64x2_t a, poly64x2_t b)
+static forceinline ATTRIBUTES uint8x16_t
+ADD_SUFFIX(clmul_high)(uint8x16_t a, poly64x2_t b)
 {
 #if defined(__clang__) && defined(ARCH_ARM64)
 	/*
 	 * Use inline asm to ensure that pmull2 is really used.  This works
 	 * around clang bug https://github.com/llvm/llvm-project/issues/52868.
 	 */
-	poly128_t res;
+	uint8x16_t res;
 
 	__asm__("pmull2 %0.1q, %1.2d, %2.2d" : "=w" (res) : "w" (a), "w" (b));
 	return res;
 #else
-	return vmull_high_p64(a, b);
+	return vreinterpretq_u8_p128(vmull_high_p64(vreinterpretq_p64_u8(a), b));
 #endif
 }
 #define clmul_high	ADD_SUFFIX(clmul_high)
@@ -73,7 +103,7 @@ ADD_SUFFIX(eor3)(uint8x16_t a, uint8x16_t b, uint8x16_t c)
 	return res;
 #endif
 #else /* ENABLE_EOR3 */
-	return a ^ b ^ c;
+	return veorq_u8(veorq_u8(a, b), c);
 #endif /* !ENABLE_EOR3 */
 }
 #define eor3	ADD_SUFFIX(eor3)
@@ -82,15 +112,10 @@ ADD_SUFFIX(eor3)(uint8x16_t a, uint8x16_t b, uint8x16_t c)
 static forceinline ATTRIBUTES uint8x16_t
 ADD_SUFFIX(fold_vec)(uint8x16_t src, uint8x16_t dst, poly64x2_t multipliers)
 {
-	/*
-	 * Using vget_low_* instead of vector indexing is necessary to avoid
-	 * poor code generation with gcc on arm32.
-	 */
-	poly128_t a = vmull_p64((poly64_t)vget_low_u8(src),
-				(poly64_t)vget_low_p64(multipliers));
-	poly128_t b = clmul_high((poly64x2_t)src, multipliers);
+	uint8x16_t a = clmul_low(src, multipliers);
+	uint8x16_t b = clmul_high(src, multipliers);
 
-	return eor3((uint8x16_t)a, (uint8x16_t)b, dst);
+	return eor3(a, b, dst);
 }
 #define fold_vec	ADD_SUFFIX(fold_vec)
 
@@ -144,7 +169,8 @@ ADD_SUFFIX(fold_partial_vec)(uint8x16_t v, const u8 *p, size_t len,
 	x0 = vtbl(v, lshift);
 
 	/* Create a vector of '16 - len' 0x00 bytes, then 'len' 0xff bytes. */
-	bsl_mask = (uint8x16_t)vshrq_n_s8((int8x16_t)rshift, 7);
+	bsl_mask = vreinterpretq_u8_s8(
+			vshrq_n_s8(vreinterpretq_s8_u8(rshift), 7));
 
 	/*
 	 * x1 = the last '16 - len' bytes from v (i.e. v right-shifted by 'len'

lib/arm/crc32_pmull_wide.h
@@ -45,7 +45,9 @@
  * Apple M1 processor is an example of such a CPU.
  */
 
-#include <arm_acle.h>
+#ifndef _MSC_VER
+#  include <arm_acle.h>
+#endif
 #include <arm_neon.h>
 
 #include "crc32_pmull_helpers.h"
@@ -53,22 +55,24 @@
 static u32 ATTRIBUTES MAYBE_UNUSED
 ADD_SUFFIX(crc32_arm)(u32 crc, const u8 *p, size_t len)
 {
-	const poly64x2_t multipliers_12 = (poly64x2_t)CRC32_12VECS_MULTS;
-	const poly64x2_t multipliers_6 = (poly64x2_t)CRC32_6VECS_MULTS;
-	const poly64x2_t multipliers_4 = (poly64x2_t)CRC32_4VECS_MULTS;
-	const poly64x2_t multipliers_3 = (poly64x2_t)CRC32_3VECS_MULTS;
-	const poly64x2_t multipliers_2 = (poly64x2_t)CRC32_2VECS_MULTS;
-	const poly64x2_t multipliers_1 = (poly64x2_t)CRC32_1VECS_MULTS;
 	uint8x16_t v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11;
 
 	if (len < 3 * 192) {
+		static const u64 _aligned_attribute(16) mults[3][2] = {
+			CRC32_4VECS_MULTS, CRC32_2VECS_MULTS, CRC32_1VECS_MULTS,
+		};
+		poly64x2_t multipliers_4, multipliers_2, multipliers_1;
+
 		if (len < 64)
 			goto tail;
+		multipliers_4 = load_multipliers(mults[0]);
+		multipliers_2 = load_multipliers(mults[1]);
+		multipliers_1 = load_multipliers(mults[2]);
 		/*
 		 * Short length; don't bother aligning the pointer, and fold
 		 * 64 bytes (4 vectors) at a time, at most.
 		 */
-		v0 = vld1q_u8(p + 0) ^ (uint8x16_t)(uint32x4_t){ crc };
+		v0 = veorq_u8(vld1q_u8(p + 0), u32_to_bytevec(crc));
 		v1 = vld1q_u8(p + 16);
 		v2 = vld1q_u8(p + 32);
 		v3 = vld1q_u8(p + 48);
@@ -92,6 +96,14 @@ ADD_SUFFIX(crc32_arm)(u32 crc, const u8 *p, size_t len)
 		}
 		v0 = fold_vec(v0, v1, multipliers_1);
 	} else {
+		static const u64 _aligned_attribute(16) mults[4][2] = {
+			CRC32_12VECS_MULTS, CRC32_6VECS_MULTS,
+			CRC32_3VECS_MULTS, CRC32_1VECS_MULTS,
+		};
+		const poly64x2_t multipliers_12 = load_multipliers(mults[0]);
+		const poly64x2_t multipliers_6 = load_multipliers(mults[1]);
+		const poly64x2_t multipliers_3 = load_multipliers(mults[2]);
+		const poly64x2_t multipliers_1 = load_multipliers(mults[3]);
 		const size_t align = -(uintptr_t)p & 15;
 		const uint8x16_t *vp;
 
@@ -114,7 +126,7 @@ ADD_SUFFIX(crc32_arm)(u32 crc, const u8 *p, size_t len)
 			len -= align;
 		}
 		vp = (const uint8x16_t *)p;
-		v0 = *vp++ ^ (uint8x16_t)(uint32x4_t){ crc };
+		v0 = veorq_u8(*vp++, u32_to_bytevec(crc));
 		v1 = *vp++;
 		v2 = *vp++;
 		v3 = *vp++;
@@ -177,8 +189,8 @@ ADD_SUFFIX(crc32_arm)(u32 crc, const u8 *p, size_t len)
 		p = (const u8 *)vp;
 	}
 	/* Reduce 128 to 32 bits using crc32 instructions. */
-	crc = __crc32d(0, (u64)vget_low_u8(v0));
-	crc = __crc32d(crc, (u64)vget_high_u8(v0));
+	crc = __crc32d(0, vgetq_lane_u64(vreinterpretq_u64_u8(v0), 0));
+	crc = __crc32d(crc, vgetq_lane_u64(vreinterpretq_u64_u8(v0), 1));
 tail:
 	/* Finish up the remainder using crc32 instructions. */
 	if (len & 32) {

commit 01d0008d3115ab18ea4baf0c4d6918ef6254c759

lib/arm/cpu_features: re-disable crc32 with buggy gcc versions
Some gcc versions (e.g. 10.2.1) return true for
__has_builtin(__builtin_arm_crc32b) but are affected by the bug
mentioned in the comment above the definition of HAVE_CRC32_INTRIN.
This causes a build error.  Make it so that the gcc version check is
still done when __has_builtin(__builtin_arm_crc32b).

Fixes https://github.com/ebiggers/libdeflate/issues/276
Fixes: 84c76f6f2cf5 ("lib/arm: make crc32 code work with MSVC")

lib/arm/cpu_features.h
@@ -137,7 +137,7 @@ static inline u32 get_arm_cpu_features(void) { return 0; }
  */
 #if HAVE_CRC32_NATIVE || \
 	(HAVE_DYNAMIC_ARM_CPU_FEATURES && \
-	 (__has_builtin(__builtin_arm_crc32b) || \
+	 ((__has_builtin(__builtin_arm_crc32b) && !GCC_PREREQ(1, 0)) || \
 	  GCC_PREREQ(11, 3) || \
 	  (GCC_PREREQ(10, 4) && !GCC_PREREQ(11, 0)) || \
 	  (GCC_PREREQ(9, 5) && !GCC_PREREQ(10, 0)) || \