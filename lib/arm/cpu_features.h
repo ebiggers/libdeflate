ebiggers/libdeflate

commit 6db64ab7afd2855c83927df8383126c96c182bce

lib/crc32: CRC-32 optimizations and other improvements
- Use the new build-time CPU feature support detection macros.

- Generate the arm32/64 implementations from a template file.

- For arm64, instantiate a pmull+crc implementation with a large stride
  length.  For now it is just enabled when macOS && arm64
  (i.e. Apple Silicon, e.g. the M1 processor), where it helps a lot.
  pmull is used to process the bulk of the data, while the crc scalar
  instructions are used for any unaligned ends.

- Also instantiate a pmull+crc+eor3 implementation.

- Use inline asm for the pmull2 instruction to ensure it gets used.

- For the arm32/arm64 crc-only implementation, enable instruction level
  parallelism by checksumming multiple chunks at once.

- Increase the stride length of x86 pclmul code from 4 to 8 vectors.

- Put the CRC32 multipliers in a shared header file.

- Many other cleanups.

lib/arm/crc32_impl.h
@@ -1,8 +1,7 @@
 /*
- * arm/crc32_impl.h
+ * arm/crc32_impl.h - ARM implementations of the gzip CRC-32 algorithm
  *
- * Copyright 2017 Jun He <jun.he@linaro.org>
- * Copyright 2018 Eric Biggers
+ * Copyright 2022 Eric Biggers
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -31,14 +30,36 @@
 
 #include "cpu_features.h"
 
-/* Implementation using ARM CRC32 instructions */
-#undef DISPATCH_ARM
-#if !defined(DEFAULT_IMPL) && \
-    (defined(__ARM_FEATURE_CRC32) || \
-     (ARM_CPU_FEATURES_ENABLED && COMPILER_SUPPORTS_CRC32_TARGET_INTRINSICS))
-#  ifdef __ARM_FEATURE_CRC32
+/* crc32_arm_pmullx4() */
+#if HAVE_PMULL_INTRIN && CPU_IS_LITTLE_ENDIAN()
+#  define SUFFIX			 _pmullx4
+#  define crc32_arm_pmullx4	crc32_arm_pmullx4
+#  define ENABLE_CRC		0
+#  define ENABLE_PMULL		1
+#  define ENABLE_EOR3		0
+#  define PMULL_STRIDE_VECS	4
+#  if HAVE_PMULL_NATIVE
 #    define ATTRIBUTES
-#    define DEFAULT_IMPL	crc32_arm
+#  else
+#    ifdef __arm__
+#      define ATTRIBUTES    __attribute__((target("fpu=crypto-neon-fp-armv8")))
+#    else
+#      ifdef __clang__
+#        define ATTRIBUTES  __attribute__((target("crypto")))
+#      else
+#        define ATTRIBUTES  __attribute__((target("+crypto")))
+#      endif
+#    endif
+#  endif
+#  include "crc32_template.h"
+#endif
+
+/* crc32_arm_crc() */
+#if HAVE_CRC32_INTRIN
+#  define SUFFIX			 _crc
+#  define crc32_arm_crc		crc32_arm_crc
+#  if HAVE_CRC32_NATIVE
+#    define ATTRIBUTES
 #  else
 #    ifdef __arm__
 #      ifdef __clang__
@@ -53,199 +74,107 @@
 #        define ATTRIBUTES	__attribute__((target("+crc")))
 #      endif
 #    endif
-#    define DISPATCH		1
-#    define DISPATCH_ARM	1
 #  endif
-
-/*
- * gcc's (as of 10.1) version of arm_acle.h for arm32, and clang's (as of
- * 10.0.1) version of arm_acle.h for both arm32 and arm64, have a bug where they
- * only define the CRC32 functions like __crc32b() when __ARM_FEATURE_CRC32 is
- * defined.  That prevents them from being used via __attribute__((target)) when
- * the main target doesn't have CRC32 support enabled.  The actual built-ins
- * like __builtin_arm_crc32b() are available and work, however; it's just the
- * wrappers in arm_acle.h like __crc32b() that erroneously don't get defined.
- * Work around this by manually defining __ARM_FEATURE_CRC32.
- */
-#ifndef __ARM_FEATURE_CRC32
-#  define __ARM_FEATURE_CRC32	1
+#  define ENABLE_CRC	1
+#  define ENABLE_PMULL	0
+#  include "crc32_template.h"
 #endif
-#include <arm_acle.h>
 
-static u32 ATTRIBUTES
-crc32_arm(u32 remainder, const u8 *p, size_t size)
-{
-	while (size != 0 && (uintptr_t)p & 7) {
-		remainder = __crc32b(remainder, *p++);
-		size--;
-	}
+/* crc32_arm_pmullx12_crc() */
+#if defined(__aarch64__) && CPU_IS_LITTLE_ENDIAN() && \
+	(HAVE_PMULL_INTRIN && HAVE_CRC32_INTRIN) && \
+	((HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE) || \
+	 (HAVE_PMULL_TARGET && HAVE_CRC32_TARGET))
+#  define SUFFIX				 _pmullx12_crc
+#  define crc32_arm_pmullx12_crc	crc32_arm_pmullx12_crc
+#  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE
+#    define ATTRIBUTES
+#  else
+#    ifdef __clang__
+#      define ATTRIBUTES  __attribute__((target("crypto,crc")))
+#    else
+#      define ATTRIBUTES  __attribute__((target("arch=armv8-a+crypto+crc")))
+#    endif
+#  endif
+#  define ENABLE_CRC		1
+#  define ENABLE_PMULL		1
+#  define ENABLE_EOR3		0
+#  define PMULL_STRIDE_VECS	12
+#  include "crc32_template.h"
+#endif
 
-	while (size >= 64) {
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 0)));
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 1)));
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 2)));
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 3)));
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 4)));
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 5)));
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 6)));
-		remainder = __crc32d(remainder, le64_bswap(*((u64 *)p + 7)));
-		p += 64;
-		size -= 64;
-	}
-
-	while (size >= 8) {
-		remainder = __crc32d(remainder, le64_bswap(*(u64 *)p));
-		p += 8;
-		size -= 8;
-	}
-
-	while (size != 0) {
-		remainder = __crc32b(remainder, *p++);
-		size--;
-	}
-
-	return remainder;
-}
-#undef ATTRIBUTES
-#endif /* Implementation using ARM CRC32 instructions */
+/* crc32_arm_pmullx12_crc_eor3() */
+#if defined(__aarch64__) && CPU_IS_LITTLE_ENDIAN() && \
+	(HAVE_PMULL_INTRIN && HAVE_CRC32_INTRIN) && \
+	((HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && HAVE_SHA3_NATIVE) || \
+	 (HAVE_PMULL_TARGET && HAVE_CRC32_TARGET && HAVE_SHA3_TARGET))
+#  define SUFFIX				 _pmullx12_crc_eor3
+#  define crc32_arm_pmullx12_crc_eor3	crc32_arm_pmullx12_crc_eor3
+#  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && HAVE_SHA3_NATIVE
+#    define ATTRIBUTES
+#  else
+#    ifdef __clang__
+#      define ATTRIBUTES  __attribute__((target("crypto,crc,sha3")))
+#    else
+#      define ATTRIBUTES  __attribute__((target("arch=armv8.2-a+crypto+crc+sha3")))
+#    endif
+#  endif
+#  define ENABLE_CRC		1
+#  define ENABLE_PMULL		1
+#  define ENABLE_EOR3		1
+#  define PMULL_STRIDE_VECS	12
+#  include "crc32_template.h"
+#endif
 
 /*
- * CRC-32 folding with ARM Crypto extension-PMULL
- *
- * This works the same way as the x86 PCLMUL version.
- * See x86/crc32_pclmul_template.h for an explanation.
+ * On the Apple M1 processor, a PMULL implementation is significantly faster
+ * than a CRC scalar implementation, provided that PMULL_STRIDE_VECS is large
+ * enough.  Increasing it all the way to 12 is helpful.
  */
-#undef DISPATCH_PMULL
-#if !defined(DEFAULT_IMPL) && \
-    (defined(__ARM_FEATURE_CRYPTO) ||	\
-     (ARM_CPU_FEATURES_ENABLED &&	\
-      COMPILER_SUPPORTS_PMULL_TARGET_INTRINSICS)) && \
-      /* not yet tested on big endian, probably needs changes to work there */ \
-    (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
-#  define FUNCNAME		crc32_pmull
-#  define FUNCNAME_ALIGNED	crc32_pmull_aligned
-#  ifdef __ARM_FEATURE_CRYPTO
-#    define ATTRIBUTES
-#    define DEFAULT_IMPL	crc32_pmull
-#  else
-#    ifdef __arm__
-#      define ATTRIBUTES	__attribute__((target("fpu=crypto-neon-fp-armv8")))
-#    else
-#      ifdef __clang__
-#        define ATTRIBUTES	__attribute__((target("crypto")))
-#      else
-#        define ATTRIBUTES	__attribute__((target("+crypto")))
-#      endif
-#    endif
-#    define DISPATCH		1
-#    define DISPATCH_PMULL	1
+#define PREFER_PMULL_TO_CRC	0
+#ifdef __APPLE__
+#  include <TargetConditionals.h>
+#  if TARGET_OS_OSX
+#    undef PREFER_PMULL_TO_CRC
+#    define PREFER_PMULL_TO_CRC	1
 #  endif
+#endif
 
-#include <arm_neon.h>
-
-static forceinline ATTRIBUTES uint8x16_t
-clmul_00(uint8x16_t a, uint8x16_t b)
-{
-	return (uint8x16_t)vmull_p64((poly64_t)vget_low_u8(a),
-				     (poly64_t)vget_low_u8(b));
-}
-
-static forceinline ATTRIBUTES uint8x16_t
-clmul_10(uint8x16_t a, uint8x16_t b)
-{
-	return (uint8x16_t)vmull_p64((poly64_t)vget_low_u8(a),
-				     (poly64_t)vget_high_u8(b));
-}
-
-static forceinline ATTRIBUTES uint8x16_t
-clmul_11(uint8x16_t a, uint8x16_t b)
-{
-	return (uint8x16_t)vmull_high_p64((poly64x2_t)a, (poly64x2_t)b);
-}
-
-static forceinline ATTRIBUTES uint8x16_t
-fold_128b(uint8x16_t dst, uint8x16_t src, uint8x16_t multipliers)
-{
-	return dst ^ clmul_00(src, multipliers) ^ clmul_11(src, multipliers);
-}
-
-static forceinline ATTRIBUTES u32
-crc32_pmull_aligned(u32 remainder, const uint8x16_t *p, size_t nr_segs)
-{
-	/* Constants precomputed by gen_crc32_multipliers.c.  Do not edit! */
-	const uint8x16_t multipliers_4 =
-		(uint8x16_t)(uint64x2_t){ 0x8F352D95, 0x1D9513D7 };
-	const uint8x16_t multipliers_1 =
-		(uint8x16_t)(uint64x2_t){ 0xAE689191, 0xCCAA009E };
-	const uint8x16_t final_multiplier =
-		(uint8x16_t)(uint64x2_t){ 0xB8BC6765 };
-	const uint8x16_t mask32 = (uint8x16_t)(uint32x4_t){ 0xFFFFFFFF };
-	const uint8x16_t barrett_reduction_constants =
-			(uint8x16_t)(uint64x2_t){ 0x00000001F7011641,
-						  0x00000001DB710641 };
-	const uint8x16_t zeroes = (uint8x16_t){ 0 };
-
-	const uint8x16_t * const end = p + nr_segs;
-	const uint8x16_t * const end512 = p + (nr_segs & ~3);
-	uint8x16_t x0, x1, x2, x3;
-
-	x0 = *p++ ^ (uint8x16_t)(uint32x4_t){ remainder };
-	if (nr_segs >= 4) {
-		x1 = *p++;
-		x2 = *p++;
-		x3 = *p++;
-
-		/* Fold 512 bits at a time */
-		while (p != end512) {
-			x0 = fold_128b(*p++, x0, multipliers_4);
-			x1 = fold_128b(*p++, x1, multipliers_4);
-			x2 = fold_128b(*p++, x2, multipliers_4);
-			x3 = fold_128b(*p++, x3, multipliers_4);
-		}
-
-		/* Fold 512 bits => 128 bits */
-		x1 = fold_128b(x1, x0, multipliers_1);
-		x2 = fold_128b(x2, x1, multipliers_1);
-		x0 = fold_128b(x3, x2, multipliers_1);
-	}
-
-	/* Fold 128 bits at a time */
-	while (p != end)
-		x0 = fold_128b(*p++, x0, multipliers_1);
-
-	/* Fold 128 => 96 bits, implicitly appending 32 zeroes */
-	x0 = vextq_u8(x0, zeroes, 8) ^ clmul_10(x0, multipliers_1);
-
-	/* Fold 96 => 64 bits */
-	x0 = vextq_u8(x0, zeroes, 4) ^ clmul_00(x0 & mask32, final_multiplier);
-
-	/* Reduce 64 => 32 bits using Barrett reduction */
-	x1 = x0;
-	x0 = clmul_00(x0 & mask32, barrett_reduction_constants);
-	x0 = clmul_10(x0 & mask32, barrett_reduction_constants);
-	return vgetq_lane_u32((uint32x4_t)(x0 ^ x1), 1);
-}
-#define IMPL_ALIGNMENT		16
-#define IMPL_SEGMENT_SIZE	16
-#include "../crc32_vec_template.h"
-#endif /* PMULL implementation */
-
-#ifdef DISPATCH
+/*
+ * If the best implementation is statically available, use it unconditionally.
+ * Otherwise choose the best implementation at runtime.
+ */
+#if PREFER_PMULL_TO_CRC && \
+	defined(crc32_arm_pmullx12_crc_eor3) && \
+	HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && HAVE_SHA3_NATIVE
+#  define DEFAULT_IMPL	crc32_arm_pmullx12_crc_eor3
+#elif !PREFER_PMULL_TO_CRC && defined(crc32_arm_crc) && HAVE_CRC32_NATIVE
+#  define DEFAULT_IMPL	crc32_arm_crc
+#else
 static inline crc32_func_t
 arch_select_crc32_func(void)
 {
-	const u32 features = get_arm_cpu_features();
+	const u32 features MAYBE_UNUSED = get_arm_cpu_features();
 
-#ifdef DISPATCH_ARM
-	if (features & ARM_CPU_FEATURE_CRC32)
-		return crc32_arm;
+#if PREFER_PMULL_TO_CRC && defined(crc32_arm_pmullx12_crc_eor3)
+	if (HAVE_PMULL(features) && HAVE_CRC32(features) && HAVE_SHA3(features))
+		return crc32_arm_pmullx12_crc_eor3;
 #endif
-#ifdef DISPATCH_PMULL
-	if (features & ARM_CPU_FEATURE_PMULL)
-		return crc32_pmull;
+#if PREFER_PMULL_TO_CRC && defined(crc32_arm_pmullx12_crc)
+	if (HAVE_PMULL(features) && HAVE_CRC32(features))
+		return crc32_arm_pmullx12_crc;
+#endif
+#ifdef crc32_arm_crc
+	if (HAVE_CRC32(features))
+		return crc32_arm_crc;
+#endif
+#ifdef crc32_arm_pmullx4
+	if (HAVE_PMULL(features))
+		return crc32_arm_pmullx4;
 #endif
 	return NULL;
 }
-#endif /* DISPATCH */
+#define arch_select_crc32_func	arch_select_crc32_func
+#endif
 
 #endif /* LIB_ARM_CRC32_IMPL_H */

lib/x86/crc32_impl.h
@@ -1,5 +1,5 @@
 /*
- * x86/crc32_impl.h - x86 implementations of CRC-32 checksum algorithm
+ * x86/crc32_impl.h - x86 implementations of the gzip CRC-32 algorithm
  *
  * Copyright 2016 Eric Biggers
  *
@@ -30,63 +30,61 @@
 
 #include "cpu_features.h"
 
-/*
- * Include the PCLMUL/AVX implementation?  Although our PCLMUL-optimized CRC-32
- * function doesn't use any AVX intrinsics specifically, it can benefit a lot
- * from being compiled for an AVX target: on Skylake, ~16700 MB/s vs. ~10100
- * MB/s.  I expect this is related to the PCLMULQDQ instructions being assembled
- * in the newer three-operand form rather than the older two-operand form.
- *
- * Note: this is only needed if __AVX__ is *not* defined, since otherwise the
- * "regular" PCLMUL implementation would already be AVX enabled.
- */
-#undef DISPATCH_PCLMUL_AVX
-#if !defined(DEFAULT_IMPL) && !defined(__AVX__) &&	\
-	X86_CPU_FEATURES_ENABLED && COMPILER_SUPPORTS_AVX_TARGET &&	\
-	(defined(__PCLMUL__) || COMPILER_SUPPORTS_PCLMUL_TARGET_INTRINSICS)
-#  define FUNCNAME		crc32_pclmul_avx
-#  define FUNCNAME_ALIGNED	crc32_pclmul_avx_aligned
-#  define ATTRIBUTES		__attribute__((target("pclmul,avx")))
-#  define DISPATCH		1
-#  define DISPATCH_PCLMUL_AVX	1
-#  include "crc32_pclmul_template.h"
-#endif
-
 /* PCLMUL implementation */
-#undef DISPATCH_PCLMUL
-#if !defined(DEFAULT_IMPL) &&	\
-	(defined(__PCLMUL__) || (X86_CPU_FEATURES_ENABLED &&	\
-				 COMPILER_SUPPORTS_PCLMUL_TARGET_INTRINSICS))
-#  define FUNCNAME		crc32_pclmul
-#  define FUNCNAME_ALIGNED	crc32_pclmul_aligned
-#  ifdef __PCLMUL__
+#if HAVE_PCLMUL_INTRIN
+#  define SUFFIX			 _pclmul
+#  define crc32_x86_pclmul	crc32_x86_pclmul
+#  if HAVE_PCLMUL_NATIVE
 #    define ATTRIBUTES
-#    define DEFAULT_IMPL	crc32_pclmul
 #  else
 #    define ATTRIBUTES		__attribute__((target("pclmul")))
-#    define DISPATCH		1
-#    define DISPATCH_PCLMUL	1
 #  endif
-#  include "crc32_pclmul_template.h"
+#  include "crc32_template.h"
 #endif
 
-#ifdef DISPATCH
+/*
+ * PCLMUL/AVX implementation.  Although the PCLMUL-optimized CRC-32 function
+ * doesn't use any AVX intrinsics specifically, it can benefit a lot from being
+ * compiled for an AVX target: on Skylake, ~16700 MB/s vs. ~10100 MB/s.  This is
+ * probably related to the PCLMULQDQ instructions being assembled in the newer
+ * three-operand form rather than the older two-operand form.
+ */
+#if HAVE_PCLMUL_INTRIN && \
+	(HAVE_PCLMUL_NATIVE && HAVE_AVX_NATIVE) || \
+	(HAVE_PCLMUL_TARGET && HAVE_AVX_TARGET)
+#  define SUFFIX			 _pclmul_avx
+#  define crc32_x86_pclmul_avx	crc32_x86_pclmul_avx
+#  if HAVE_PCLMUL_NATIVE && HAVE_AVX_NATIVE
+#    define ATTRIBUTES
+#  else
+#    define ATTRIBUTES		__attribute__((target("pclmul,avx")))
+#  endif
+#  include "crc32_template.h"
+#endif
+
+/*
+ * If the best implementation is statically available, use it unconditionally.
+ * Otherwise choose the best implementation at runtime.
+ */
+#if defined(crc32_x86_pclmul_avx) && HAVE_PCLMUL_NATIVE && HAVE_AVX_NATIVE
+#define DEFAULT_IMPL	crc32_x86_pclmul_avx
+#else
 static inline crc32_func_t
 arch_select_crc32_func(void)
 {
-	const u32 features = get_x86_cpu_features();
+	const u32 features MAYBE_UNUSED = get_x86_cpu_features();
 
-#ifdef DISPATCH_PCLMUL_AVX
-	if ((features & X86_CPU_FEATURE_PCLMUL) &&
-	    (features & X86_CPU_FEATURE_AVX))
-		return crc32_pclmul_avx;
+#ifdef crc32_x86_pclmul_avx
+	if (HAVE_PCLMUL(features) && HAVE_AVX(features))
+		return crc32_x86_pclmul_avx;
 #endif
-#ifdef DISPATCH_PCLMUL
-	if (features & X86_CPU_FEATURE_PCLMUL)
-		return crc32_pclmul;
+#ifdef crc32_x86_pclmul
+	if (HAVE_PCLMUL(features))
+		return crc32_x86_pclmul;
 #endif
 	return NULL;
 }
-#endif /* DISPATCH */
+#define arch_select_crc32_func	arch_select_crc32_func
+#endif
 
 #endif /* LIB_X86_CRC32_IMPL_H */

commit a6562f0ac84e985ba0828919878eb0c9039fee6f

lib/arm/crc32: use crypto target when required due to gcc bug
Fixes https://github.com/ebiggers/libdeflate/issues/280
Fixes: 6db64ab7afd2 ("lib/crc32: CRC-32 optimizations and other improvements")

lib/arm/cpu_features.h
@@ -116,6 +116,29 @@ static inline u32 get_arm_cpu_features(void) { return 0; }
 #else
 #  define HAVE_PMULL_INTRIN	0
 #endif
+/*
+ * Set USE_PMULL_TARGET_EVEN_IF_NATIVE if a workaround for a gcc bug that was
+ * fixed by commit 11a113d501ff ("aarch64: Simplify feature definitions") in gcc
+ * 13 is needed.  A minimal program that fails to build due to this bug when
+ * compiled with -mcpu=emag, at least with gcc 10 through 12, is:
+ *
+ *    static inline __attribute__((always_inline,target("+crypto"))) void f() {}
+ *    void g() { f(); }
+ *
+ * The error is:
+ *
+ *    error: inlining failed in call to ‘always_inline’ ‘f’: target specific option mismatch
+ *
+ * The workaround is to explicitly add the crypto target to the non-inline
+ * function g(), even though this should not be required due to -mcpu=emag
+ * enabling 'crypto' natively and causing __ARM_FEATURE_CRYPTO to be defined.
+ */
+#if HAVE_PMULL_NATIVE && defined(ARCH_ARM64) && \
+		GCC_PREREQ(6, 1) && !GCC_PREREQ(13, 1)
+#  define USE_PMULL_TARGET_EVEN_IF_NATIVE	1
+#else
+#  define USE_PMULL_TARGET_EVEN_IF_NATIVE	0
+#endif
 
 /* CRC32 */
 #ifdef __ARM_FEATURE_CRC32

lib/arm/crc32_impl.h
@@ -236,7 +236,7 @@ crc32_arm_crc(u32 crc, const u8 *p, size_t len)
  * for implementations that use pmull for folding the data itself.
  */
 #if HAVE_CRC32_INTRIN && HAVE_PMULL_INTRIN
-#  if HAVE_CRC32_NATIVE && HAVE_PMULL_NATIVE
+#  if HAVE_CRC32_NATIVE && HAVE_PMULL_NATIVE && !USE_PMULL_TARGET_EVEN_IF_NATIVE
 #    define ATTRIBUTES
 #  else
 #    ifdef ARCH_ARM32
@@ -438,7 +438,7 @@ crc32_arm_crc_pmullcombine(u32 crc, const u8 *p, size_t len)
 #if HAVE_PMULL_INTRIN
 #  define crc32_arm_pmullx4	crc32_arm_pmullx4
 #  define SUFFIX			 _pmullx4
-#  if HAVE_PMULL_NATIVE
+#  if HAVE_PMULL_NATIVE && !USE_PMULL_TARGET_EVEN_IF_NATIVE
 #    define ATTRIBUTES
 #  else
 #    ifdef ARCH_ARM32
@@ -558,7 +558,7 @@ crc32_arm_pmullx4(u32 crc, const u8 *p, size_t len)
 #if defined(ARCH_ARM64) && HAVE_PMULL_INTRIN && HAVE_CRC32_INTRIN
 #  define crc32_arm_pmullx12_crc	crc32_arm_pmullx12_crc
 #  define SUFFIX				 _pmullx12_crc
-#  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE
+#  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && !USE_PMULL_TARGET_EVEN_IF_NATIVE
 #    define ATTRIBUTES
 #  else
 #    ifdef __clang__
@@ -584,7 +584,8 @@ crc32_arm_pmullx4(u32 crc, const u8 *p, size_t len)
 	(HAVE_SHA3_TARGET || HAVE_SHA3_NATIVE)
 #  define crc32_arm_pmullx12_crc_eor3	crc32_arm_pmullx12_crc_eor3
 #  define SUFFIX				 _pmullx12_crc_eor3
-#  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && HAVE_SHA3_NATIVE
+#  if HAVE_PMULL_NATIVE && HAVE_CRC32_NATIVE && HAVE_SHA3_NATIVE && \
+	!USE_PMULL_TARGET_EVEN_IF_NATIVE
 #    define ATTRIBUTES
 #  else
 #    ifdef __clang__