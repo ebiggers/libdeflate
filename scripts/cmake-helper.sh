736f40e4374f2b33dae18fc7c1d54faf6f5feb08
Eric Biggers <ebiggers3@gmail.com>
2023-03-23 19:07:56 -0700

    scripts/cmake-helper.sh: add 'set -e'

--- scripts/cmake-helper.sh
+++ scripts/cmake-helper.sh
@@ -3,11 +3,13 @@
 # This script ensures that the 'build' directory has been created and configured
 # with the given CMake options and environment.
 
+set -e
+
 TOPDIR="$(dirname "$0")"/..
 BUILDDIR="$TOPDIR"/build
 
 flags=$(env; echo "@CMAKEOPTS@=$*")
-if [ "$flags" != "$(cat "$BUILDDIR"/.flags 2>/dev/null)" ]; then
+if [ "$flags" != "$(cat "$BUILDDIR"/.flags 2>/dev/null || true)" ]; then
 	rm -rf "$BUILDDIR"/CMakeCache.txt "$BUILDDIR"/CMakeFiles
 	mkdir -p "$BUILDDIR"
 	cmake -S "$TOPDIR" -B "$BUILDDIR" "$@"
