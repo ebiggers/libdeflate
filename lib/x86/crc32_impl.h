ebiggers/libdeflate

commit 5f2a0b4beca9ff017ba016ee38a55b580fa66206

lib/x86/crc32: add VPCLMULQDQ implementations of CRC-32
This improves CRC-32 performance on some of the latest x86 CPUs.  Three
implementations are added: VPCLMULQDQ/AVX2 and VPCLMULQDQ/AVX512VL which
use 256-bit vectors, and VPCLMULQDQ/AVX512F/AVX512VL which uses 512-bit
vectors.  To reduce downclocking effects, the implementation with
512-bit vectors isn't used on Intel CPUs 10th generation and older.

lib/x86/crc32_impl.h
@@ -30,67 +30,137 @@
 
 #include "cpu_features.h"
 
-/* PCLMUL implementation */
-#if HAVE_PCLMUL_INTRIN
-#  define crc32_x86_pclmul	crc32_x86_pclmul
-#  define SUFFIX			 _pclmul
-#  if HAVE_PCLMUL_NATIVE
+/* PCLMULQDQ implementation */
+#if HAVE_PCLMULQDQ_INTRIN
+#  define crc32_x86_pclmulqdq	crc32_x86_pclmulqdq
+#  define SUFFIX			 _pclmulqdq
+#  if HAVE_PCLMULQDQ_NATIVE
 #    define ATTRIBUTES
 #  else
 #    define ATTRIBUTES		_target_attribute("pclmul")
 #  endif
-#  define FOLD_PARTIAL_VECS	0
+#  define VL			16
+#  define FOLD_LESSTHAN16BYTES	0
+#  define USE_TERNARYLOGIC	0
 #  include "crc32_pclmul_template.h"
 #endif
 
 /*
- * PCLMUL/AVX implementation.  This implementation has two benefits over the
- * regular PCLMUL one.  First, simply compiling against the AVX target can
- * improve performance significantly (e.g. 10100 MB/s to 16700 MB/s on Skylake)
- * without actually using any AVX intrinsics, probably due to the availability
- * of non-destructive VEX-encoded instructions.  Second, AVX support implies
- * SSSE3 and SSE4.1 support, and we can use SSSE3 and SSE4.1 intrinsics for
- * efficient handling of partial blocks.  (We *could* compile a variant with
- * PCLMUL+SSSE3+SSE4.1 w/o AVX, but for simplicity we don't currently bother.)
+ * PCLMULQDQ/AVX implementation.  Compared to the regular PCLMULQDQ
+ * implementation, this still uses 128-bit vectors, but it has two potential
+ * benefits.  First, simply compiling against the AVX target can improve
+ * performance significantly (e.g. 10100 MB/s to 16700 MB/s on Skylake) without
+ * actually using any AVX intrinsics, probably due to the availability of
+ * non-destructive VEX-encoded instructions.  Second, AVX support implies SSSE3
+ * and SSE4.1 support, and we can use SSSE3 and SSE4.1 intrinsics for efficient
+ * handling of partial blocks.  (We *could* compile a variant with
+ * PCLMULQDQ+SSE4.1 without AVX, but for simplicity we don't currently bother.)
  *
  * FIXME: with MSVC, this isn't actually compiled with AVX code generation
  * enabled yet.  That would require that this be moved to its own .c file.
  */
-#if HAVE_PCLMUL_INTRIN && HAVE_AVX_INTRIN
-#  define crc32_x86_pclmul_avx	crc32_x86_pclmul_avx
-#  define SUFFIX			 _pclmul_avx
-#  if HAVE_PCLMUL_NATIVE && HAVE_AVX_NATIVE
+#if HAVE_PCLMULQDQ_INTRIN && HAVE_AVX_INTRIN
+#  define crc32_x86_pclmulqdq_avx	crc32_x86_pclmulqdq_avx
+#  define SUFFIX				 _pclmulqdq_avx
+#  if HAVE_PCLMULQDQ_NATIVE && HAVE_AVX_NATIVE
 #    define ATTRIBUTES
 #  else
 #    define ATTRIBUTES		_target_attribute("pclmul,avx")
 #  endif
-#  define FOLD_PARTIAL_VECS	1
+#  define VL			16
+#  define FOLD_LESSTHAN16BYTES	1
+#  define USE_TERNARYLOGIC	0
+#  include "crc32_pclmul_template.h"
+#endif
+
+/* VPCLMULQDQ/AVX2 implementation.  Uses 256-bit vectors. */
+#if HAVE_VPCLMULQDQ_INTRIN && HAVE_PCLMULQDQ_INTRIN && HAVE_AVX2_INTRIN && \
+	/*
+	 * This has to be disabled on MSVC because MSVC has a bug where it
+	 * incorrectly assumes that VPCLMULQDQ implies AVX-512:
+	 * https://developercommunity.visualstudio.com/t/Compiler-incorrectly-assumes-VAES-and-VP/10578785?space=62&q=AVX512&sort=newest
+	 */ \
+	!(defined(_MSC_VER) && !defined(__clang__))
+#  define crc32_x86_vpclmulqdq_avx2	crc32_x86_vpclmulqdq_avx2
+#  define SUFFIX				 _vpclmulqdq_avx2
+#  if HAVE_VPCLMULQDQ_NATIVE && HAVE_PCLMULQDQ_NATIVE && HAVE_AVX2_NATIVE
+#    define ATTRIBUTES
+#  else
+#    define ATTRIBUTES		_target_attribute("vpclmulqdq,pclmul,avx2")
+#  endif
+#  define VL			32
+#  define FOLD_LESSTHAN16BYTES	1
+#  define USE_TERNARYLOGIC	0
 #  include "crc32_pclmul_template.h"
 #endif
 
 /*
- * If the best implementation is statically available, use it unconditionally.
- * Otherwise choose the best implementation at runtime.
+ * VPCLMULQDQ/AVX512VL implementation.  This takes advantage of some AVX-512
+ * instructions but uses 256-bit vectors rather than 512-bit.  This can be
+ * useful on CPUs where 512-bit vectors cause downclocking.
  */
-#if defined(crc32_x86_pclmul_avx) && HAVE_PCLMUL_NATIVE && HAVE_AVX_NATIVE
-#define DEFAULT_IMPL	crc32_x86_pclmul_avx
-#else
+#if HAVE_VPCLMULQDQ_INTRIN && HAVE_PCLMULQDQ_INTRIN && HAVE_AVX512VL_INTRIN
+#  define crc32_x86_vpclmulqdq_avx512vl	crc32_x86_vpclmulqdq_avx512vl
+#  define SUFFIX				 _vpclmulqdq_avx512vl
+#  if HAVE_VPCLMULQDQ_NATIVE && HAVE_PCLMULQDQ_NATIVE && HAVE_AVX512VL_NATIVE
+#    define ATTRIBUTES
+#  else
+#    define ATTRIBUTES		_target_attribute("vpclmulqdq,pclmul,avx512vl")
+#  endif
+#  define VL			32
+#  define FOLD_LESSTHAN16BYTES	1
+#  define USE_TERNARYLOGIC	1
+#  include "crc32_pclmul_template.h"
+#endif
+
+/* VPCLMULQDQ/AVX512F/AVX512VL implementation.  Uses 512-bit vectors. */
+#if HAVE_VPCLMULQDQ_INTRIN && HAVE_PCLMULQDQ_INTRIN && \
+	HAVE_AVX512F_INTRIN && HAVE_AVX512VL_INTRIN
+#  define crc32_x86_vpclmulqdq_avx512f_avx512vl	crc32_x86_vpclmulqdq_avx512f_avx512vl
+#  define SUFFIX					 _vpclmulqdq_avx512f_avx512vl
+#if HAVE_VPCLMULQDQ_NATIVE && HAVE_PCLMULQDQ_NATIVE && \
+	HAVE_AVX512F_NATIVE && HAVE_AVX512VL_NATIVE
+#    define ATTRIBUTES
+#  else
+#    define ATTRIBUTES		_target_attribute("vpclmulqdq,pclmul,avx512f,avx512vl")
+#  endif
+#  define VL			64
+#  define FOLD_LESSTHAN16BYTES	1
+#  define USE_TERNARYLOGIC	1
+#  include "crc32_pclmul_template.h"
+#endif
+
+/* Choose the best implementation at runtime. */
 static inline crc32_func_t
 arch_select_crc32_func(void)
 {
 	const u32 features MAYBE_UNUSED = get_x86_cpu_features();
 
-#ifdef crc32_x86_pclmul_avx
-	if (HAVE_PCLMUL(features) && HAVE_AVX(features))
-		return crc32_x86_pclmul_avx;
+#ifdef crc32_x86_vpclmulqdq_avx512f_avx512vl
+	if (HAVE_VPCLMULQDQ(features) && HAVE_PCLMULQDQ(features) &&
+	    HAVE_AVX512F(features) && HAVE_AVX512VL(features))
+		return crc32_x86_vpclmulqdq_avx512f_avx512vl;
 #endif
-#ifdef crc32_x86_pclmul
-	if (HAVE_PCLMUL(features))
-		return crc32_x86_pclmul;
+#ifdef crc32_x86_vpclmulqdq_avx512vl
+	if (HAVE_VPCLMULQDQ(features) && HAVE_PCLMULQDQ(features) &&
+	    HAVE_AVX512VL(features))
+		return crc32_x86_vpclmulqdq_avx512vl;
+#endif
+#ifdef crc32_x86_vpclmulqdq_avx2
+	if (HAVE_VPCLMULQDQ(features) && HAVE_PCLMULQDQ(features) &&
+	    HAVE_AVX2(features))
+		return crc32_x86_vpclmulqdq_avx2;
+#endif
+#ifdef crc32_x86_pclmulqdq_avx
+	if (HAVE_PCLMULQDQ(features) && HAVE_AVX(features))
+		return crc32_x86_pclmulqdq_avx;
+#endif
+#ifdef crc32_x86_pclmulqdq
+	if (HAVE_PCLMULQDQ(features))
+		return crc32_x86_pclmulqdq;
 #endif
 	return NULL;
 }
 #define arch_select_crc32_func	arch_select_crc32_func
-#endif
 
 #endif /* LIB_X86_CRC32_IMPL_H */

commit 1a6f436edd37998e1452341dffd03a8cfd7ab3b0

lib/x86/crc32: fix undefined behavior in VPCLMULQDQ optimized functions
The specifications for _mm256_castsi128_si256() and
_mm512_castsi128_si512() are bugged, as they leave the high bits
undefined instead of zeroed as would be expected.  Separate intrinsics
_mm256_zextsi128_si256() and _mm512_zextsi128_si512() were later added
to allow working around this defect.  Use them.

This fixes incorrect CRC checksums produced by
crc32_x86_vpclmulqdq_avx512_vl512() when built with 'clang -O0'.
Other cases are not known to have been affected.

Resolves https://github.com/ebiggers/libdeflate/issues/403
Fixes: 5f2a0b4beca9 ("lib/x86/crc32: add VPCLMULQDQ implementations of CRC-32")

lib/x86/crc32_impl.h
@@ -78,8 +78,10 @@ static const u8 MAYBE_UNUSED shift_tab[48] = {
  *
  * gcc 8.1 and 8.2 had a similar bug where they assumed that
  * _mm256_clmulepi64_epi128() always needed AVX512.  It's fixed in gcc 8.3.
+ *
+ * _mm256_zextsi128_si256() requires gcc 10.
  */
-#if (GCC_PREREQ(8, 3) || CLANG_PREREQ(6, 0, 10000000)) && \
+#if (GCC_PREREQ(10, 1) || CLANG_PREREQ(6, 0, 10000000)) && \
 	!defined(LIBDEFLATE_ASSEMBLER_DOES_NOT_SUPPORT_VPCLMULQDQ)
 #  define crc32_x86_vpclmulqdq_avx2	crc32_x86_vpclmulqdq_avx2
 #  define SUFFIX				 _vpclmulqdq_avx2
@@ -89,7 +91,7 @@ static const u8 MAYBE_UNUSED shift_tab[48] = {
 #  include "crc32_pclmul_template.h"
 #endif
 
-#if (GCC_PREREQ(8, 1) || CLANG_PREREQ(6, 0, 10000000) || MSVC_PREREQ(1920)) && \
+#if (GCC_PREREQ(10, 1) || CLANG_PREREQ(6, 0, 10000000) || MSVC_PREREQ(1920)) && \
 	!defined(LIBDEFLATE_ASSEMBLER_DOES_NOT_SUPPORT_VPCLMULQDQ)
 /*
  * VPCLMULQDQ/AVX512 implementation using 256-bit vectors.  This is very similar
@@ -97,6 +99,8 @@ static const u8 MAYBE_UNUSED shift_tab[48] = {
  * instruction and more registers.  This is used on CPUs that support AVX-512
  * but where using 512-bit vectors causes downclocking.  This should also be the
  * optimal implementation on CPUs that support AVX10/256 but not AVX10/512.
+ *
+ * _mm256_zextsi128_si256() requires gcc 10.
  */
 #  define crc32_x86_vpclmulqdq_avx512_vl256  crc32_x86_vpclmulqdq_avx512_vl256
 #  define SUFFIX				      _vpclmulqdq_avx512_vl256
@@ -109,6 +113,8 @@ static const u8 MAYBE_UNUSED shift_tab[48] = {
  * VPCLMULQDQ/AVX512 implementation using 512-bit vectors.  This is used on CPUs
  * that have a good AVX-512 implementation including VPCLMULQDQ.  This should
  * also be the optimal implementation on CPUs that support AVX10/512.
+ *
+ * _mm512_zextsi128_si512() requires gcc 10.
  */
 #  define crc32_x86_vpclmulqdq_avx512_vl512  crc32_x86_vpclmulqdq_avx512_vl512
 #  define SUFFIX				      _vpclmulqdq_avx512_vl512

lib/x86/crc32_pclmul_template.h
@@ -80,7 +80,7 @@
 #  define fold_vec		fold_vec256
 #  define VLOADU(p)		_mm256_loadu_si256((const void *)(p))
 #  define VXOR(a, b)		_mm256_xor_si256((a), (b))
-#  define M128I_TO_VEC(a)	_mm256_castsi128_si256(a)
+#  define M128I_TO_VEC(a)	_mm256_zextsi128_si256(a)
 #  define MULTS(a, b)		_mm256_set_epi64x(a, b, a, b)
 #  define MULTS_8V		MULTS(CRC32_X2015_MODG, CRC32_X2079_MODG)
 #  define MULTS_4V		MULTS(CRC32_X991_MODG, CRC32_X1055_MODG)
@@ -91,7 +91,7 @@
 #  define fold_vec		fold_vec512
 #  define VLOADU(p)		_mm512_loadu_si512((const void *)(p))
 #  define VXOR(a, b)		_mm512_xor_si512((a), (b))
-#  define M128I_TO_VEC(a)	_mm512_castsi128_si512(a)
+#  define M128I_TO_VEC(a)	_mm512_zextsi128_si512(a)
 #  define MULTS(a, b)		_mm512_set_epi64(a, b, a, b, a, b, a, b)
 #  define MULTS_8V		MULTS(CRC32_X4063_MODG, CRC32_X4127_MODG)
 #  define MULTS_4V		MULTS(CRC32_X2015_MODG, CRC32_X2079_MODG)