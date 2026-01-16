2573fbd1c9d747add481f918abd6cf94d9013a10
Eric Biggers <ebiggers@google.com>
2026-01-15 18:29:57 -0800

    gen-release-archives.sh: stop building 32-bit Windows binaries
    It's time to stop providing prebuilt 32-bit Windows binaries.

--- scripts/gen-release-archives.sh
+++ scripts/gen-release-archives.sh
@@ -13,25 +13,23 @@ echo "Generating $tarball"
 git archive --format=tar --prefix="${prefix}/" HEAD \
 	| libdeflate-gzip -12 > "$tarball"
 
-# Generate Windows binary releases libdeflate-*-windows-*-bin.zip
-for arch in 'i686' 'x86_64'; do
-	dir=${prefix}-windows-${arch}-bin
-	zipfile="${dir}.zip"
-	echo "Generating $zipfile"
-	rm -rf build "$dir" "$zipfile"
-	CFLAGS="-Werror" ${arch}-w64-mingw32-cmake -B build -G Ninja \
-		-DLIBDEFLATE_BUILD_TESTS=1 > /dev/null
-	cmake --build build > /dev/null
-	mkdir "$dir"
-	cp libdeflate.h build/libdeflate.{dll,dll.a,a} \
-		build/programs/{benchmark,checksum}.exe "$dir"
-	cp build/programs/libdeflate-gzip.exe "$dir"/gzip.exe
-	cp build/programs/libdeflate-gzip.exe "$dir"/gunzip.exe
-	${arch}-w64-mingw32-strip "$dir"/libdeflate.dll "$dir"/*.exe
-	for file in COPYING NEWS.md README.md; do
-		sed < $file > "$dir/${file}.txt" -e 's/$/\r/g'
-	done
-	(cd "$dir" && zip -q -r "../${zipfile}" .)
+# Generate Windows binary release libdeflate-*-windows-x86_64-bin.zip
+dir=${prefix}-windows-x86_64-bin
+zipfile="${dir}.zip"
+echo "Generating $zipfile"
+rm -rf build "$dir" "$zipfile"
+CFLAGS="-Werror" x86_64-w64-mingw32-cmake -B build -G Ninja \
+	-DLIBDEFLATE_BUILD_TESTS=1 > /dev/null
+cmake --build build > /dev/null
+mkdir "$dir"
+cp libdeflate.h build/libdeflate.{dll,dll.a,a} \
+	build/programs/{benchmark,checksum}.exe "$dir"
+cp build/programs/libdeflate-gzip.exe "$dir"/gzip.exe
+cp build/programs/libdeflate-gzip.exe "$dir"/gunzip.exe
+x86_64-w64-mingw32-strip "$dir"/libdeflate.dll "$dir"/*.exe
+for file in COPYING NEWS.md README.md; do
+	sed < $file > "$dir/${file}.txt" -e 's/$/\r/g'
 done
+(cd "$dir" && zip -q -r "../${zipfile}" .)
 
 echo "Successfully generated release archives"
